7bc615e680edb4c12e68e2dd2dd0ad12772dcab3
syaroh <rohimatulmaisyaroh@gmail.com>
2024-05-27 22:41:42 +0700

    menambah fungsi samadengan (=), fungsi operator, dan fungsi titik atau koma

--- App.jsx
+++ App.jsx
@@ -17,13 +17,30 @@ class App extends Component {
     };
   }
 
+  // fungsi operator
   masukkanAngka = value => {
     if (this.state.hitung == 0) {
       this.setState({hitung: value});
     } else {
-      this.setState({hitung: value + '' + this.state.hitung});
+      this.setState({hitung: this.state.hitung + '' + value});
     }
   };
+
+  // fungsi hasil atau sama dengan
+  hitungHasil = () => {
+    let hasil = eval(this.state.hitung);
+    this.setState({hitung: hasil});
+  };
+
+  // fungsi koma atau titik
+  titik = () => {
+    if (this.state.hitung == 0) {
+      this.setState({hitung: 0});
+    } else {
+      this.setState({hitung: this.state.hitung + '' + '.'});
+    }
+  };
+
   render() {
     return (
       <View style={styles.background}>
@@ -59,11 +76,12 @@ class App extends Component {
                 flex: 1,
                 padding: 10,
                 margin: 10,
-              }}>
+              }}
+              onPress={() => this.setState({hitung: 0})}>
               <Text
                 style={{
                   textAlign: 'right',
-                  color: 'white',
+                  color: 'orange',
                   fontSize: 24,
                   padding: 10,
                 }}>
@@ -71,19 +89,27 @@ class App extends Component {
               </Text>
             </TouchableOpacity>
           </View>
-          {/* penutup tampilan view 'celar' */}
+          {/* penutup tampilan view 'clear' */}
 
           <View style={styles.bgBaris}>
-            <TouchableOpacity style={styles.bgAngka}>
-              <Text style={styles.angka}>(</Text>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(0)}>
+              <Text style={styles.angka}>0</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
-              <Text style={styles.angka}>)</Text>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.titik()}>
+              <Text style={styles.angka}>.</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
-              <Text style={styles.angka}>/</Text>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka('/')}>
+              <Text style={styles.operator}>/</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka('x')}>
               <Text style={styles.operator}>x</Text>
             </TouchableOpacity>
           </View>
@@ -94,43 +120,65 @@ class App extends Component {
               onPress={() => this.masukkanAngka(7)}>
               <Text style={styles.angka}>7</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(8)}>
               <Text style={styles.angka}>8</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(9)}>
               <Text style={styles.angka}>9</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka('+')}>
               <Text style={styles.operator}>+</Text>
             </TouchableOpacity>
           </View>
 
           <View style={styles.bgBaris}>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(4)}>
               <Text style={styles.angka}>4</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(5)}>
               <Text style={styles.angka}>5</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(6)}>
               <Text style={styles.angka}>6</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka('-')}>
               <Text style={styles.operator}>-</Text>
             </TouchableOpacity>
           </View>
 
           <View style={styles.bgBaris}>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(1)}>
               <Text style={styles.angka}>1</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(2)}>
               <Text style={styles.angka}>2</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.masukkanAngka(3)}>
               <Text style={styles.angka}>3</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bgAngka}>
+            <TouchableOpacity
+              style={styles.bgAngka}
+              onPress={() => this.hitungHasil()}>
               <Text style={styles.operator}>=</Text>
             </TouchableOpacity>
           </View>
